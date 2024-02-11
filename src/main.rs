2dc266e4a8af94ddadf4daf091954d59eeb3302b
Jeff Garzik <jgarzik@pobox.com>
2024-02-11 16:50:37 -0500

    Validate PCC file input against schema

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,14 @@ struct Args {
     datadir: String,
 }
 
+enum TagType {
+    Bool,
+    Date,
+    Number,
+    Text,
+    ReadPcc,
+}
+
 #[derive(Clone)]
 pub struct PccConfig {
     datadir: String,
@@ -28,6 +36,7 @@ pub struct PccConfig {
 pub struct Pcc {
     config: PccConfig,
     dict: HashMap<String, String>,
+    pcc_schema: HashMap<String, TagType>,
 }
 
 impl Pcc {
@@ -35,14 +44,41 @@ impl Pcc {
         Pcc {
             config: config.clone(),
             dict: HashMap::new(),
+            pcc_schema: HashMap::from([
+                (String::from("!PRECAMPAIGN"), TagType::Text),
+                (String::from("BOOKTYPE"), TagType::Text),
+                (String::from("CAMPAIGN"), TagType::Text),
+                (String::from("COPYRIGHT"), TagType::Text),
+                (String::from("COVER"), TagType::Text),
+                (String::from("FORWARDREF"), TagType::Text),
+                (String::from("GAMEMODE"), TagType::Text),
+                (String::from("GENRE"), TagType::Text),
+                (String::from("INFOTEXT"), TagType::Bool),
+                (String::from("ISOGL"), TagType::Bool),
+                (String::from("ISLICENSED"), TagType::Bool),
+                (String::from("KEY"), TagType::Text),
+                (String::from("LOGO"), TagType::Text),
+                (String::from("PCC"), TagType::ReadPcc),
+                (String::from("PUBNAMELONG"), TagType::Text),
+                (String::from("PUBNAMESHORT"), TagType::Text),
+                (String::from("PUBNAMEWEB"), TagType::Text),
+                (String::from("RANK"), TagType::Number),
+                (String::from("SETTING"), TagType::Text),
+                (String::from("SHOWINMENU"), TagType::Text),
+                (String::from("SOURCEDATE"), TagType::Date),
+                (String::from("SOURCELONG"), TagType::Text),
+                (String::from("SOURCESHORT"), TagType::Text),
+                (String::from("SOURCEWEB"), TagType::Text),
+                (String::from("STATUS"), TagType::Text),
+                (String::from("TYPE"), TagType::Text),
+                (String::from("URL"), TagType::Text),
+            ]),
         }
     }
 
     pub fn read(&mut self, relpath: &str) -> io::Result<()> {
         let mut abspath = PathBuf::from(&self.config.datadir);
         abspath.push(relpath);
-
-        println!("PCC-read-lines({})", abspath.as_path().display());
 
         let file = File::open(abspath)?;
         let rdr = BufReader::new(file);
@@ -58,6 +94,9 @@ impl Pcc {
                     match sor {
                         None => return Err(Error::new(ErrorKind::Other, "PCC invalid line:colon")),
                         Some((lhs, rhs)) => {
+                            if !self.pcc_schema.contains_key(lhs) {
+                                return Err(Error::new(ErrorKind::Other, "PCC invalid key"));
+                            }
                             self.dict.insert(lhs.to_string(), rhs.to_string());
                         }
                     }
